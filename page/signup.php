c47df87010fe03f6cbd8a09118d8dac1ab1e99bc
freckie <freckie@frec.kr>
2018-03-28 11:03:59 +0900

    sign up developing

--- page/signup.php
+++ page/signup.php
@@ -1,4 +1,4 @@
-<!-- login.php -->
+<!-- signup.php -->
 
 <!DOCTYPE html>
 <html>
@@ -8,7 +8,7 @@
 	<meta http-equiv="content-type" content="text/html; charset=UTF-8">
 
 	<!-- Title -->
-	<title>다솜 교육 포털</title>
+	<title>다솜 교육 포털 :: 회원가입</title>
 
 	<!-- CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
@@ -18,7 +18,6 @@
 	<script src="//code.jquery.com/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
 	<script src="./../js/background_slide.js"></script>
-	<script src="./../js/login_control.js"></script>
 </head>
 
 <body>
@@ -45,27 +44,10 @@
 					<div>
 						<input class="form-control input-box font-rixm" type="text" placeholder="ID" id="username" name="username" required />
 						<input class="form-control input-box" type="password" placeholder="PW" id="userpw" name="userpw" required />
-						<button class="form-control btn btn-primary input-box font-rixm" id="login-btn">Log In</button>
+						<button class="form-control btn btn-primary input-box font-rixm" id="login-btn">Sign Up</button>
 					</div>
 
 			</div>
-
-			<!-- Login Alert Modal -->
-			<div class="modal fade bs-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true" id="login-alert-modal">
-				<div class="modal-dialog modal-sm">
-					<div class="modal-content">
-						<div class="modal-header">
-							<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-								<span aria-hidden="true">×</span>
-							</button>
-							<h4 class="modal-title" id="alert-modal-label">로그인 실패!</h4>
-						</div>
-						<div class="modal-body" style="text-align: center">
-							<span class="font-rixm" id="alert-modal-content">일치하는 정보가 없습니다.</span>
-						</div>
-					</div> <!-- /.modal-content -->
-				</div> <!-- /.modal-dialog -->
-			</div> <!-- /.modal -->
 
 		</div>
 	</div>
