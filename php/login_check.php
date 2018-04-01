86a5b8644224c3c38fa45d78431f8e3ca3d1ac11
freckie <freckie@frec.kr>
2018-04-01 14:20:37 +0900

    login bug fixed

--- php/login_check.php
+++ php/login_check.php
@@ -24,16 +24,23 @@ if($result = $conn->query($sql))
 	$row = $result->fetch_assoc();
 	if(!strcmp($row['pw'], md5($pw)))
 	{
-		session_start();
+		if($row['admission'] == 0)
+		{
+			$response['status'] = 'non-admit';
+		}
+		else
+		{
+			session_start();
 
-		$_SESSION['user_idx'] = $row['idx'];
-		$_SESSION['user_id'] = $row['id'];
-		$_SESSION['user_name'] = $row['name'];
-		$_SESSION['user_year'] = $row['year'];
-		$_SESSION['user_mail'] = $row['mail'];
-		$_SESSION['user_num'] = $row['num'];
+			$_SESSION['user_idx'] = $row['idx'];
+			$_SESSION['user_id'] = $row['id'];
+			$_SESSION['user_name'] = $row['name'];
+			$_SESSION['user_year'] = $row['year'];
+			$_SESSION['user_mail'] = $row['mail'];
+			$_SESSION['user_num'] = $row['number'];
 
-		$response['status'] = 'success';
+			$response['status'] = 'success';
+		}
 	}
 	else
 	{
